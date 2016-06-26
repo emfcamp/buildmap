5909d5f1988a20ccaed88fa2ea2215c94c61d8b4
Russ Garrett <russ@garrett.co.uk>
2016-06-26 16:40:05 +0100

    Run regeneration in another thread so webhook doesn't time out

--- webhook-server.py
+++ webhook-server.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import division, absolute_import, print_function, unicode_literals
 import os
+import threading
 import subprocess
 from flask import Flask
 from flask.ext.hookserver import Hooks
@@ -8,10 +9,19 @@ import config
 
 script_location = os.path.dirname(os.path.abspath(__file__))
 
+thread = None
 app = Flask(__name__)
-#app.config['VALIDATE_IP'] = False
+# app.config['VALIDATE_IP'] = False
 app.config['VALIDATE_SIGNATURE'] = False
 hooks = Hooks(app, url='/hooks/')
+
+
+def regenerate_worker():
+    for filename in config.source_files.values():
+        os.chdir(os.path.abspath(os.path.dirname(os.path.join(script_location, filename))))
+        subprocess.check_call(['git', 'pull'])
+    os.chdir(script_location)
+    subprocess.check_call(['python', './buildmap.py'])
 
 
 @hooks.hook('ping')
@@ -21,9 +31,8 @@ def ping(data, guid):
 
 @hooks.hook('push')
 def push(data, guid):
-    for filename in config.source_files.values():
-        os.chdir(os.path.abspath(os.path.dirname(os.path.join(script_location, filename))))
-        subprocess.check_call(['git', 'pull'])
-    os.chdir(script_location)
-    subprocess.check_call(['python', './buildmap.py'])
+    global thread
+    if thread is None or not thread.is_alive():
+        thread = threading.Thread(name='worker', target=regenerate_worker)
+        thread.start()
     return 'OK'
