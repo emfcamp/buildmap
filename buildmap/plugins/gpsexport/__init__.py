03e4383c5bee2f706925811d0f73002ba5764ead
dpslwk <dps.lwk@gmail.com>
2022-04-14 12:02:20 +0100

    GpsexportPlugin update

--- buildmap/plugins/gpsexport/__init__.py
+++ buildmap/plugins/gpsexport/__init__.py
@@ -1,26 +1,33 @@
+from __future__ import absolute_import
 import csv
 import datetime
 import logging
 import os
 import shutil
 import time
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, PackageLoader, select_autoescape
 from sqlalchemy import text
 
-from util import write_file
+# from util import write_file
+def write_file(name, data):
+    with open(name, "w") as fp:
+        fp.write(data)
+
+
 from . import exportsql
 
 
-class GPSExport(object):
-    def __init__(self, _, config, db):
+class GpsexportPlugin(object):
+    def __init__(self, buildmap, _config, opts, db):
         self.log = logging.getLogger(__name__)
         self.queries = exportsql.queries
-        self.config = config
-        self.base_path = os.path.dirname(os.path.abspath(__file__))
-        self.temp_dir = os.path.join(self.base_path, "temp")
         self.db = db
-        shutil.rmtree(self.temp_dir, True)
-        os.makedirs(self.temp_dir)
+        self.buildmap = buildmap
+        self.opts = opts
+        self.env = Environment(
+            loader=PackageLoader("buildmap.plugins.gpsexport", "templates"),
+            autoescape=select_autoescape(["html", "xml"]),
+        )
 
     def run_query(self, query):
         return self.db.execute(text(query)).fetchall()
@@ -28,8 +35,7 @@ class GPSExport(object):
     def generate_kml(self, dir, name, places):
         if "kml" not in places[0].keys():
             return
-        env = Environment(loader=PackageLoader("buildmap", "templates"))
-        template = env.get_template("kml.jinja")
+        template = self.env.get_template("kml.jinja")
         write_file(os.path.join(dir, name + ".kml"), template.render(places=places))
         if name in self.filesToList:
             self.filesToList[name].append("kml")
@@ -38,7 +44,7 @@ class GPSExport(object):
 
     def generate_csv(self, dir, name, places):
         with open(os.path.join(dir, name + ".csv"), "w") as csvfile:
-            fieldnames = places[0].keys()
+            fieldnames = list(places[0].keys())
             fieldnames.sort()
             if "name" in fieldnames:
                 fieldnames.insert(0, fieldnames.pop(fieldnames.index("name")))
@@ -52,48 +58,41 @@ class GPSExport(object):
                 self.filesToList[name] = ["csv"]
 
     def generate_html(self, dir, files):
-        env = Environment(loader=PackageLoader("buildmap", "templates"))
-        template = env.get_template("export-html.jinja")
-        write_file(os.path.join(dir, "export.html"), template.render(files=files))
+        template = self.env.get_template("export-html.jinja")
+        write_file(
+            os.path.join(dir, "export.html"),
+            template.render(files=files, name=self.opts.get("name")),
+        )
 
     def run(self):
         start_time = time.time()
         self.log.info("Generating map GPS exports...")
-        kmlDir = self.config.output_directory + "/kml"
-        tempKmlDir = kmlDir + "-" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        oldKmlDir = kmlDir + "-old"
-        csvDir = self.config.output_directory + "/csv"
-        tempCsvDir = csvDir + "-" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-        oldCsvDir = csvDir + "-old"
+
+        out_path = self.buildmap.resolve_path(self.buildmap.config["web_directory"])
+
+        kmlDir = out_path + "/kml"
+        csvDir = out_path + "/csv"
 
         results = {}
 
         self.log.info("Getting map data")
-        for name, query in self.queries.iteritems():
+        for name, query in self.queries.items():
             self.log.info("Fetching %s" % (name))
             results[name] = self.run_query(query)
 
         self.log.info("Generating kml/csv files...")
-        os.mkdir(tempKmlDir)
-        os.mkdir(tempCsvDir)
+
+        if not os.path.isdir(kmlDir):
+            os.makedirs(kmlDir)
+        if not os.path.isdir(csvDir):
+            os.makedirs(csvDir)
+
         self.filesToList = {}
-        for name, places in results.iteritems():
-            self.generate_kml(tempKmlDir, name, places)
-            self.generate_csv(tempCsvDir, name, places)
-
-        self.log.info("Moving new kml into place...")
-        shutil.rmtree(oldKmlDir, True)
-        if os.path.exists(kmlDir):
-            shutil.move(kmlDir, oldKmlDir)
-        shutil.move(tempKmlDir, kmlDir)
-
-        self.log.info("Moving new csv into place...")
-        shutil.rmtree(oldCsvDir, True)
-        if os.path.exists(csvDir):
-            shutil.move(csvDir, oldCsvDir)
-        shutil.move(tempCsvDir, csvDir)
+        for name, places in results.items():
+            self.generate_kml(kmlDir, name, places)
+            self.generate_csv(csvDir, name, places)
 
         self.log.info("Generaring html links page")
-        self.generate_html(self.config.output_directory, self.filesToList)
+        self.generate_html(out_path, self.filesToList)
 
         self.log.info("Generation complete in %.2f seconds", time.time() - start_time)
